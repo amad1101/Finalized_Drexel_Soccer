91b1f9baac17e9139a01c4ea4da0ee75640a269e
Amadou Diallo <amadia95@gmail.com>
2019-09-22 13:00:30 -0400

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,11 +1,24 @@
 const cron = require('node-cron');
 
-const test = require("./src/index")
+// const test = require("./src/index")
 const { db } = require("./src/db")
 
 // update the db each hours
-cron.schedule(' 0 * * * * *', async () => {
+cron.schedule(' * * * * *', async () => {
   
+  const { DateTime } = require("luxon");
+  date2 = DateTime.local().setZone('America/New_York').plus({ days: 1 }).toISODate();
+  date = DateTime.local().setZone('America/New_York').toISODate();
+
+  console.log('====================================');
+  console.log(` today ${date}`);
+  console.log('====================================');
+
+  console.log('====================================');
+  console.log(` tomorow ${date2}`);
+  console.log('====================================');
+  // console.log(typeof(date));
+  // console.log(date2);
   const fetch = require('node-fetch');
   //Fetch fuction
   async function getFieldAvailability( startDate , endDate, roomID = "2163" ) {
@@ -27,12 +40,54 @@ cron.schedule(' 0 * * * * *', async () => {
     const bookings = JSON.parse(JSON.parse(jsons.d).JsonData).bookings
     return bookings;
   };
+
+  
+// Helper Function that get two days events inputs and classify them depending on the day. output => today:[todayEvents], tomorrow:[tomorrowEvents].
+async function todayActivities(data) {
+  // return data
+  reservation = await data;
+  let datas = {};
+  // console.log(data.length);
+  if(reservation.length  > 0){
+    for (var i = 0; i < reservation.length; i++) {
+      let startActivity = reservation[i].Start.toString();
+      // console.log(startActivity);
+      // return startActivity;
+      if (startActivity.includes(date)) {
+          if (!datas.today) {
+            datas.today = [reservation[i]]
+          }
+          else {
+            datas.today.push(reservation[i])
+          }
+      // console.log(data[i]);
+      }
+      else if(startActivity.includes(date2)) {
+        if (!datas.tomorow) {
+          datas.tomorow = [reservation[i]]
+        }
+        else {
+          datas.tomorow.push(reservation[i])
+        }
+      }
+      // console.log(datas);
+      else {
+        continue
+      }
+    //
+    }
+    return datas
+  }
+  else{
+    return ""
+  }
+};
    
   // remenber to put all the functions and global objects inside
-  let end = test.DateGen(1);
-  let start  = test.DateGen(0);
-  let data = await getFieldAvailability(start, end)
-  let helperFun = await test.TodayActivities(data)
+  // let end = test.DateGen(1);
+  // let start  = test.DateGen(0);
+  let data = await getFieldAvailability(date, date2)
+  let helperFun = await todayActivities(data)
   
   console.log('====================================');
   console.log(helperFun);
