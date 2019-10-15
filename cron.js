aa7f851fd9f7c125f8ce83ae7d0219bb135f741e
Amadou Diallo <amadia95@gmail.com>
2019-10-15 14:55:53 -0400

    Updated fetch
    Drexel updated their ems api link. Threfore added new link.

--- cron.js
+++ cron.js
@@ -22,18 +22,18 @@ cron.schedule(' 0 * * * *', async () => {
   const fetch = require('node-fetch');
   //Fetch fuction
   async function getFieldAvailability( startDate , endDate, roomID = "2163" ) {
-    const response = await fetch("http://ems.drexel.edu/ServerApi.aspx/GetLocationDetailsAvailability",
-      {"credentials":"include",
-      "headers":{"accept":"application/json, text/javascript, */*; q=0.01",
-      "accept-language":"en-US,en;q=0.9,fr;q=0.8",
-      "content-type":"application/json; charset=UTF-8",
-      "dea-csrftoken":"71ec1aab-37a5-45bc-a4dd-a87319836648",
-      "x-requested-with":"XMLHttpRequest"},
-      "referrer":"http://ems.drexel.edu/BrowseForSpace.aspx",
-      "referrerPolicy":"no-referrer-when-downgrade",
-      "body":`{\"roomId\":${roomID},\"start\":\"${startDate}T04:00:00.000Z\",\"end\":\"${endDate}T04:00:00.000Z\"}`,
-      "method":"POST","mode":"cors"
-    });
+    const response = await fetch("https://ems.drexel.edu/EMSwebapp/ServerApi.aspx/GetLocationDetailsAvailability", 
+    {"credentials":"include",
+    "headers":{"accept":"application/json, text/javascript, */*; q=0.01",
+    "accept-language":"en-US,en;q=0.9,fr;q=0.8",
+    "content-type":"application/json; charset=UTF-8",
+    "dea-csrftoken":"db0881df-692a-4c4b-a14b-e1fe5fba7d91",
+    "sec-fetch-mode":"cors","sec-fetch-site":"same-origin",
+    "x-requested-with":"XMLHttpRequest"},
+    "referrer":"https://ems.drexel.edu/EMSwebapp/BrowseForSpace.aspx",
+    "referrerPolicy":"no-referrer-when-downgrade",
+    "body":`{\"roomId\":${roomID},\"start\":\"${startDate}T04:00:00.000Z\",\"end\":\"${endDate}T04:00:00.000Z\"}`,
+    "method":"POST","mode":"cors"});
   
     let jsons = await response.json();
   
